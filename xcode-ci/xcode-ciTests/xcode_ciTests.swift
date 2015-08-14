c4a52ce8a6ed6ee39950cc35133ba8d31fc021c9
Marko Justinek <mjustinek@dius.com.au>
2015-08-14 10:10:55 +1000

    Project setup

---
+++ xcode-ci/xcode-ciTests/xcode_ciTests.swift
@@ -0,0 +1,36 @@
+//
+//  xcode_ciTests.swift
+//  xcode-ciTests
+//
+//  Created by Marko DiUS on 14/08/2015.
+//  Copyright © 2015 Marko DiUS. All rights reserved.
+//
+
+import XCTest
+@testable import xcode_ci
+
+class xcode_ciTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
